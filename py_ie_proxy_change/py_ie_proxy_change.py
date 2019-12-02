0d4e421f2fb43162b38011599e41ffc375a2d2de
dasarisasidhar <dasarisasidhar.d@gmail.com>
2019-12-02 15:00:04 +0530

    Add project files.

---
+++ py_ie_proxy_change/py_ie_proxy_change.py
@@ -0,0 +1,27 @@
+import pyautogui
+import time
+
+def ie():
+    pyautogui.click(x=412, y=1058)
+    time.sleep(3)
+    pyautogui.click(x=1906, y=56)
+    time.sleep(2)
+    pyautogui.click(x=1685, y=420)
+    time.sleep(1)
+    pyautogui.click(x=358, y=87)
+    time.sleep(1)
+    pyautogui.click(x=358, y=87)
+    time.sleep(1)
+    pyautogui.click(x=510, y=606)
+    time.sleep(1)
+    pyautogui.click(x=91, y=246)
+    time.sleep(1)
+    pyautogui.click(x=401, y=514)
+    time.sleep(1)
+    pyautogui.click(x=320, y=766)
+    time.sleep(1)
+    pyautogui.click(x=1893, y=28)
+    time.sleep(1)
+    #print(pyautogui.postion())
+ie()
+
